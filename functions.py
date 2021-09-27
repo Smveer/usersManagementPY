3cb99a4030c0a67b06d9ba2f47f96af9d650e80d
Mukil <mukil.george@icloud.com>
2021-09-27 21:00:00 +0200

    after exp create pwd

--- functions.py
+++ functions.py
@@ -29,17 +29,36 @@ def generate_pwd():
     # print("The encryption password:", hashed)
     return hashed
 
-def expired_pwd()
+def expired_pwd():
     now = datetime.datetime.now()
     one_month = calendar.monthrange(now.year, now.month)[1]
     end = now + timedelta(days=one_month)
-
-    print("Current date: ")
-    print(now.strftime("%d-%m-%Y"))
-    print("1 Month: ")
+    # print ("You started the company on : " )
+    # print (now.strftime("%d-%m-%Y"))
+    print("You can change your password on  ")
     print(end.strftime("%d-%m-%Y"))
 
-    print("Your password is expired can you change please: ", )
+def create_pwd(pwd):
+    print("Your password must contain at least 8 characters,  1 lowercase letter, 1 capital letter, 1 digit and special characters.\n")
+    print("Now enter your password.\n")
+    lower, upper, special, digit = 0, 0, 0, 0
+    if (len(pwd) >= 8):
+        for i in pwd:
+            if (i.isupper()):
+                upper += 1
+            if (i.islower()):
+                lower += 1
+            if (i.isdigit()):
+                digit += 1
+            for j in string.punctuation:
+                if i == j:
+                    special += 1
+    else:
+        print("Password should be more than 8 characters")
+    if (lower >= 1 and upper >= 1 and special >= 1 and digit >= 1):
+        print("Valid Password")
+    else:
+        print("Password not valid. Please try again")
 
 
 def verify_date(day, month, year):
