531c2f5e5ab25f6ae41b41bae76f21c671437f5f
manveer singh <singh.manveer93@icloud.com>
2021-10-01 02:10:50 +0200

    user FSananes sign in

--- tests/signInTest.py
+++ tests/signInTest.py
@@ -4,4 +4,5 @@ from functions import *
 
 print(sign_in_as_admin("MSingh", "passwOrd2!"))
 print(sign_in_as_worker("AAnonymous", "passwOrd2!"))
+print(sign_in_as_worker("FSananes", "I~+Us+}g%wWg"))
 print(create_hash("passwOrd2!"))
